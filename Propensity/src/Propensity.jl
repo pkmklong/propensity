7f3b98670e7ac1894bf1d196c8251d3074969b63
Patrick <21268383+pkmklong@users.noreply.github.com>
2021-02-02 18:15:38 -0800

    Update Propensity.jl

--- Propensity/src/Propensity.jl
+++ Propensity/src/Propensity.jl
@@ -7,6 +7,7 @@ using GLM
 using DataFrames
 using Distributions
 using Gadfly
+using StatsBase:sample
 
 # Module test
 greet() = print("Hello World!")
@@ -22,8 +23,38 @@ function assign_formula(y, df)
 end
   
 
-function sample_majority(df)
+# split to minority and majority class
+# record size of minority class (n)
+function split_by_class(df, target)
+    target = Symbol(target)
+    df_1 = df[df[target] .== 1, :]
+    df_0 = df[df[target] .== 0, :]
+    min_cls_size = size(df_1)[1]
+    return df_1, df_0, min_cls_size
 end
+
+  
+# Generate n trainings sets 
+# concat minority and n subsampled majorities
+function make_train_sets(df_0, df_1, n_sets)
+    
+    min_cls_size = size(df_1)[1]
+    
+    samples = []
+    
+    for i in 1:n_sets
+        subsample = sample(1:nrow(df_0), 
+            min_cls_size,
+            replace=false)
+        
+        df_0_sampled = df_0[subsample, :]
+        df_train = [df_0_sampled; df_1]
+        
+        samples = vcat(samples, df_train)
+    end
+    return samples
+end
+
 
 function train_n_models(n)
 end
